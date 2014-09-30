713b79451da9007045ebc8262fa770d2c9488183
Giele Cools <GieleCools@MacBook-Pro-van-Giele-Cools.local>
2014-09-30 09:38:08 +0200

    Bestand met eventuele foutjes in cursus toegevoegd

---
+++ oplossing_opdracht-foreach-Deel1V2.php
@@ -0,0 +1,16 @@
+<?php
+	$text = file_get_contents("text-file-opdracht-foreach.txt");
+	$textChars = str_split($text);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>

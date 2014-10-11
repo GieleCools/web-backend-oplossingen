d9325c4178330acd80f31b12cc2b6df726064cc0
Giele Cools <GieleCools@MacBook-Pro-van-Giele-Cools.local>
2014-10-11 15:04:54 +0200

    Oplossing opdracht-recursive afgewerkt.

---
+++ oplossing_opdracht-recursive.php
@@ -0,0 +1,44 @@
+<?php
+	$rente = (8/100);
+	$bedrag = 100000;
+	$jaar = 10;
+	$arrResult = array();
+
+	function berekenBedrag($initBedrag, $initRente, $initJaar)
+	{
+		$arrString = array();
+
+		for ($i=1; $i <=$initJaar ; $i++) 
+		{ 
+			$winst = $initBedrag*$initRente;
+			$initBedrag += ($initBedrag*$initRente);
+			$arrString[$i] = "Na ".$i." jaar is het bedrag ".floor($initBedrag)." en de winst ".floor($winst); 
+		}
+
+		return $arrString;
+	}
+
+	$arrResult = berekenBedrag($bedrag, $rente, $jaar);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Oplossing opdracht-recursiveV2</title>
+</head>
+<body>
+	<pre>
+		<!-- <?php var_dump($arrResult); ?> -->
+	</pre>
+	<p>
+		<?php foreach ($arrResult as $key => $value): ?>
+			<li>
+				<?php echo $value; ?>
+			</li>
+		<?php endforeach ?>
+	</p>
+</body>
+</html>

84bca6c1d02706edbdecc056f2359655631c93e4
Giele Cools <GieleCools@MacBook-Pro-van-Giele-Cools.local>
2014-11-13 15:17:00 +0100

    Oplossing opdracht include-require afgewerkt

---
+++ oplossing_opdracht-include-require/index.php
@@ -0,0 +1,32 @@
+<?php
+	$artikels = array(	array(	'titel' => 	'Overval Colruyt',
+								'text' 	=> 	'In Chapelle-lez-Herlaimont, in de provincie Henegouwen, 
+											hebben drie gemaskerde mannen maandagavond een filiaal van het warenhuis 
+											Colruyt overvallen, zo meldt de lokale politie.
+											Bij de overval vielen geen gewonden.
+											De daders vielen de winkel rond 19 uur binnen en bedreigden het personeel met hun wapens. 
+											Ze eisten de inhoud van de kassa en verlieten vervolgens snel de plaats van de overval. 
+											Over een eventuele buit is voorlopig niets bekend.',
+								'tags' 	=> 	array('overval', 'Colruyt')
+						), 		
+						array(	'titel' => 	'Provincie Antwerpen omdopen tot Midden-Brabant',
+								'text'	=> 	'De Vlaamse meerderheidspartijen N-VA, CD&amp;V en Open VLD willen de provincie Antwerpen omdopen in Midden-Brabant. 
+											Volgens hen logisch in het kader van de ontvoogding van de "grootsteden" Antwerpen en Gent. 
+											Steden kunnen volgens het regeerakkoord uit de provincie stappen en zelf de provinciale bevoegdheden uitvoeren. 
+											In dat geval zou het absurd zijn dat de provincie Antwerpen die naam behoudt, luidt het.
+											"Het regeerakkoord laat toe dat de grootsteden Antwerpen en Gent zich volledig ontvoogden van de provincies. 
+											In de praktijk komt het erop neer dat ze uit het provinciale verband stappen. Antwerpen ligt dan de facto buiten het 
+											provinciale werkingsgebied. Om verwarring te vermijden is deze evolutie het juiste moment om onze provincie een nieuwe 
+											naam te bezorgen", zeggen Vlaams Parlementsleden Kris Van Dijck (N-VA), Koen Van den Heuvel (CD&amp;V) en Bart Somers 
+											(Open VLD) in een mededeling.',
+								'tags'	=>	array('Antwerpen', 'politiek', 'provincie')
+
+						), 
+				);
+	
+	$cssLink = 'css/style.css';
+
+	include 'view/header-partial.html';
+	include 'view/body-partial.html';
+	include 'view/footer-partial.html';
+?>
